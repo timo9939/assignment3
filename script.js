f5cda050184177b09d621c76494a58673b05a496
timo9939 <timothylau20199@gmail.com>
2022-10-16 13:30:49 +0100

    if function updated 1.0

--- script.js
+++ script.js
@@ -17,25 +17,40 @@ var generateBtn = document.querySelector("#generate");
 // main function start
 function generatePassword(){
   len =prompt("Enter the length of your password\nThe length must be between 8 and 128");
+  
+  if(len<8 || len>128 ){
+    alert("The length of password must be between 8 and 128 characters"); 
+  };
+  if(len<8 || len>128 ) {
+   return 
+  }
+  
+
+
 
   islowercase = confirm("Do you want to include lowercase letter?");
   isuppercase = confirm("Do you want to include uppercase letter?");
   isnumeric = confirm("Do you want to include number?");
   isspecial = confirm("Do you want to include special character?");
 
-  if (!islowercase||!isuppercase||!isnumeric||!isspecial){
+  if (!islowercase && !isuppercase && !isnumeric && !isspecial){
     alert("Invalid Criteria!\nYou need to choose at least one character type")
   }
 
+  
 
-
-  if (islowercase ||isuppercase||isnumeric||isspecial){
+  if (islowercase && isuppercase && isnumeric && isspecial){
   for(var i=0;i<len;i++){
     randomNumber = Math.floor(Math.random()*alphabet.length);
     password += alphabet.substring(randomNumber,randomNumber+1);
   }}
-
+  return password;
 }
+
+
+
+
+
 
 
 
